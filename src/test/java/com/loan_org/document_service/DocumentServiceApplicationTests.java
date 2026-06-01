79f0ca3680aa8547512497988c9c1fc89ea7a970
Aman Raj <billygates124356@gmail.com>
2026-06-01 18:38:59 +0530

    Added intial commit

---
+++ src/test/java/com/loan_org/document_service/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_org.document_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
