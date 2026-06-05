d4c58cf35fac1b54c84c0d57719d33bce24d71d7
Aman Raj <billygates124356@gmail.com>
2026-06-05 23:56:21 +0530

    updated and added the fetcher methods to fetch documents

--- src/main/java/com/loan_org/document_service/document/port/DocumentRepository.java
+++ src/main/java/com/loan_org/document_service/document/port/DocumentRepository.java
@@ -2,6 +2,9 @@ package com.loan_org.document_service.document.port;
 
 import com.loan_org.document_service.document.model.DocumentMetadata;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Defines a port service saying that we can manipulate the document data in the desired
  * database, as defined in the function signatures.
@@ -20,6 +23,29 @@ public interface DocumentRepository {
      * @param data The data to save in database
      * @return The data with updated id
      */
-    DocumentMetadata save(DocumentMetadata data);
+    DocumentMetadata           save(DocumentMetadata data);
 
+    /**
+     * Fetches the document bu the unique ID
+     *
+     * @param id The id of the database
+     * @return The document with the id
+     */
+    Optional<DocumentMetadata> findById(String id);
+
+    /**
+     * Fetches all the documents for the given application ID
+     *
+     * @param applicationId The application ID to search for
+     * @return The list of document data for the given application ID
+     */
+    List<DocumentMetadata>     findByApplicationId(String applicationId);
+
+    /**
+     * Fetches the document by the storage Key
+     *
+     * @param storageKey The storage Key for the application
+     * @return The document corresponding to the storage key
+     */
+    Optional<DocumentMetadata> findByStorageKey(String storageKey);
 }
--- src/main/java/com/loan_org/document_service/infrastructure/persistence/mongo/MongoDocumentRepository.java
+++ src/main/java/com/loan_org/document_service/infrastructure/persistence/mongo/MongoDocumentRepository.java
@@ -5,6 +5,9 @@ import com.loan_org.document_service.document.port.DocumentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MongoDocumentRepository implements DocumentRepository {
@@ -14,6 +17,21 @@ public class MongoDocumentRepository implements DocumentRepository {
     @Override
     public DocumentMetadata save(DocumentMetadata data) {
         return mongoRepository.save(data);
+    }
+
+    @Override
+    public Optional<DocumentMetadata> findById(String id) {
+        return mongoRepository.findById(id);
+    }
+
+    @Override
+    public List<DocumentMetadata> findByApplicationId(String applicationId) {
+        return mongoRepository.findByApplicationId(applicationId);
+    }
+
+    @Override
+    public Optional<DocumentMetadata> findByStorageKey(String storageKey) {
+        return mongoRepository.findByStorageKey(storageKey);
     }
 
 }
