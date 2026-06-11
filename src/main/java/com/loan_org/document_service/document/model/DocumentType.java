b6843d4ab8592f994910e175b7a280da5ed2270a
Aman Raj <billygates124356@gmail.com>
2026-06-11 20:51:33 +0530

    removed the comments

--- src/main/java/com/loan_org/document_service/document/model/DocumentType.java
+++ src/main/java/com/loan_org/document_service/document/model/DocumentType.java
@@ -1,10 +1,5 @@
 package com.loan_org.document_service.document.model;
 
-/**
- * Document type sent for verification
- *
- * @author amanfoundongithub
- */
 public enum DocumentType {
     AADHAAR,
     PAN,
