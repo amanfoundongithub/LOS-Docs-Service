6991f8c0a0f8173ea67744f82c34381280ca915b
Aman Raj <billygates124356@gmail.com>
2026-06-11 21:17:50 +0530

    added an exception for storage stream

---
+++ src/main/java/com/loan_org/document_service/document/exception/StorageStreamException.java
@@ -0,0 +1,7 @@
+package com.loan_org.document_service.document.exception;
+
+public class StorageStreamException extends RuntimeException {
+    public StorageStreamException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
