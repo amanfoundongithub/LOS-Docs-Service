509858fd4998667811492c768cdb64f953501481
Aman Raj <billygates124356@gmail.com>
2026-06-11 11:43:17 +0530

    added document type

---
+++ src/main/java/com/loan_org/document_service/document/model/DocumentType.java
@@ -0,0 +1,12 @@
+package com.loan_org.document_service.document.model;
+
+/**
+ * Document type sent for verification
+ *
+ * @author amanfoundongithub
+ */
+public enum DocumentType {
+    AADHAAR,
+    PAN,
+    OTHER
+}
