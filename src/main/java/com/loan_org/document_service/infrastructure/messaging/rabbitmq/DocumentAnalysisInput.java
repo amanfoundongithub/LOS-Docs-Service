ccb2d4d33c35b5461fbb5ee87a4599f44db89b9f
Aman Raj <billygates124356@gmail.com>
2026-06-12 00:41:37 +0530

    added document type & key

--- src/main/java/com/loan_org/document_service/infrastructure/messaging/rabbitmq/DocumentAnalysisInput.java
+++ src/main/java/com/loan_org/document_service/infrastructure/messaging/rabbitmq/DocumentAnalysisInput.java
@@ -2,5 +2,6 @@ package com.loan_org.document_service.infrastructure.messaging.rabbitmq;
 
 public record DocumentAnalysisInput(
         String documentId,
-        String storageKey
+        String storageKey,
+        String documentType
 ) {}
