3efa134b8a727068bf389550233dd3f6fb8ad407
Aman Raj <billygates124356@gmail.com>
2026-06-11 20:16:35 +0530

    updated valid content type with description

--- src/main/java/com/loan_org/document_service/document/decorator/ValidContentType.java
+++ src/main/java/com/loan_org/document_service/document/decorator/ValidContentType.java
@@ -1,10 +1,24 @@
 package com.loan_org.document_service.document.decorator;
 
+import com.loan_org.document_service.infrastructure.decorator.ContentTypeValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
+/**
+ * Defines the interface for the validation of contentType parameter in the request. Use this
+ * annotator to decide on the valid content type that should be present in the request. All the
+ * DTOs that require the "contentType" will pass the MIME and the decorator will match it against the
+ * available ones.
+ * <p>
+ *     Decorator implementation is available in the `infrastructure/decorator` folder for reference.
+ * </p>
+ *
+ *
+ * @author amanfoundongithub
+ * @version 2.0.0
+ */
 @Documented
 @Constraint(validatedBy = ContentTypeValidator.class)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
