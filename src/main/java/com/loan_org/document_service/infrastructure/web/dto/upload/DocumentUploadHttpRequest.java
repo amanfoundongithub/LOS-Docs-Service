8941a57b6402ea6871a2e904debd8b3c48c8dcfb
Aman Raj <billygates124356@gmail.com>
2026-06-18 10:20:44 +0530

    renamed to document validation rules

--- src/main/java/com/loan_org/document_service/infrastructure/web/dto/upload/DocumentUploadHttpRequest.java
+++ src/main/java/com/loan_org/document_service/infrastructure/web/dto/upload/DocumentUploadHttpRequest.java
@@ -1,6 +1,6 @@
 package com.loan_org.document_service.infrastructure.web.dto.upload;
 
-import com.loan_org.document_service.document.constraints.ValidationConstants;
+import com.loan_org.document_service.document.constraints.DocumentValidationRules;
 import com.loan_org.document_service.document.constraints.decorator.ValidateContentType;
 import com.loan_org.document_service.document.model.DocumentType;
 import jakarta.validation.constraints.Min;
@@ -11,23 +11,23 @@ import lombok.Data;
 @Data
 public class DocumentUploadHttpRequest {
 
-    @NotBlank(message = ValidationConstants.APPLICATION_ID_NOT_BLANK_MESSAGE)
+    @NotBlank(message = DocumentValidationRules.APPLICATION_ID_NOT_BLANK_MESSAGE)
     private String applicationId;
 
-    @NotNull(message = ValidationConstants.DOCUMENT_TYPE_NOT_BLANK_MESSAGE)
+    @NotNull(message = DocumentValidationRules.DOCUMENT_TYPE_NOT_BLANK_MESSAGE)
     private DocumentType documentType;
 
-    @NotBlank(message = ValidationConstants.CUSTOMER_ID_NOT_BLANK_MESSAGE)
+    @NotBlank(message = DocumentValidationRules.CUSTOMER_ID_NOT_BLANK_MESSAGE)
     private String       customerId;
 
-    @NotBlank(message = ValidationConstants.CONTENT_TYPE_NOT_BLANK_MESSAGE)
-    @ValidateContentType(message = ValidationConstants.CONTENT_TYPE_NOT_VALID_MESSAGE)
+    @NotBlank(message = DocumentValidationRules.CONTENT_TYPE_NOT_BLANK_MESSAGE)
+    @ValidateContentType(message = DocumentValidationRules.CONTENT_TYPE_NOT_VALID_MESSAGE)
     private String       contentType;
 
-    @NotBlank(message = ValidationConstants.FILE_NAME_NOT_BLANK_MESSAGE)
+    @NotBlank(message = DocumentValidationRules.FILE_NAME_NOT_BLANK_MESSAGE)
     private String fileName;
 
-    @Min(value = ValidationConstants.MIN_FILE_SIZE_IN_BYTES,
-            message = ValidationConstants.FILE_MINIMUM_SIZE_MESSAGE)
+    @Min(value = DocumentValidationRules.MIN_FILE_SIZE_IN_BYTES,
+            message = DocumentValidationRules.FILE_MINIMUM_SIZE_MESSAGE)
     private Long fileSize;
 }
